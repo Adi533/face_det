41688553009167a66bb2ebb93f152c99cb2f1c0b
Aditya <adityasharma10102@gmail.com>
2024-06-22 14:14:10 +0530

    second

--- faceDetection.py
+++ faceDetection.py
@@ -1,25 +1,3 @@
-# import cv2
-
-# def detect_face(imagePath):
-#     img = cv2.imread(imagePath)
-#     if img is None:
-#         print(f"Error: Unable to read image file {imagePath}")
-#         return False
-#     gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-#     face_classifier = cv2.CascadeClassifier(
-#         cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
-#     )
-#     face = face_classifier.detectMultiScale(gray_image, scaleFactor=1.1, minNeighbors=5, minSize=(40, 40))
-#     return len(face) > 0
-
-# imagePath = './images/image6.jpg'
-# result = detect_face(imagePath)
-# if result:
-#     print("Face Detected")
-# else:
-#     print("No Face Detected")
-
-
 import cv2
 import numpy as np
 import urllib.request
